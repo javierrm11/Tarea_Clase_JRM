e573c16a377aff4bdc33bd64a583e9eedbb42ee6
jose.gomez <javierrumo2@gmail.com>
2024-05-29 18:40:12 +0200

    Programa terminado, añadiendo un menu para hacer todas las funciones

--- src/main/java/ejercicio/Main.java
+++ src/main/java/ejercicio/Main.java
@@ -2,6 +2,7 @@ package ejercicio;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -9,9 +10,33 @@ public class Main {
 
         Aplicacion ap = new Aplicacion(cn);
 
+        Scanner scanner = new Scanner(System.in);
 
-        ap.mostrarDietasDeInformatica();
+        while (true) {
+            System.out.println("1. Agregar Dieta");
+            System.out.println("2. Mostrar Diedas de Informatica");
+            System.out.println("3. Incrementar Precio");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            int opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    ap.agregaDieta();
+                    break;
+                case 2:
+                    ap.mostrarDietasDeInformatica();
+                    break;
+                case 3:
+                    ap.incrementarPrecioVentas();
+                    break;
+                case 4:
+                    scanner.close();
+                    return;
 
-        ap.incrementarPrecioVentas();
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        }
     }
 }
